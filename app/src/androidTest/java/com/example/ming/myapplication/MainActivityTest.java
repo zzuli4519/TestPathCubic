274051acb07547f0251c3feeb3788bd2d614ea60
zzuli4519 <zzuli4519@163.com>
2016-01-11 15:23:35 +0800

    open test activity

--- app/src/androidTest/java/com/example/ming/myapplication/MainActivityTest.java
+++ app/src/androidTest/java/com/example/ming/myapplication/MainActivityTest.java
@@ -6,17 +6,19 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.test.InstrumentationTestCase;
 
 /**
+ * 测试启动Activity
  * Created by Ming on 2016/1/11.
  */
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
-    public MainActivityTest(Class<MainActivity> activityClass) {
-        super("com.example.ming.myapplication", activityClass);
+    public MainActivityTest() {
+        super(MainActivity.class);
     }
 
     public void testLogin(){
         Intent mIntent=new Intent();
         mIntent.setClassName("com.example.ming.myapplication","com.example.ming.myapplication.MainActivity");
+        mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         Instrumentation mInstrumentation=getInstrumentation();
         mInstrumentation.startActivitySync(mIntent);
     }
