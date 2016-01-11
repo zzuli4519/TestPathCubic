3565127947c03b54c207010bbf7d3a9550354e34
zzuli4519 <zzuli4519@163.com>
2016-01-11 11:22:17 +0800

    test  application

--- app/src/androidTest/java/com/example/ming/myapplication/ApplicationTest.java
+++ app/src/androidTest/java/com/example/ming/myapplication/ApplicationTest.java
@@ -2,6 +2,8 @@ package com.example.ming.myapplication;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -10,4 +12,13 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    /**
+     * 测试执行Test Android测试工程类
+     */
+    public void testFindAll(){
+        Log.e("tag----test","error");
+    }
+
+
 }
