1bd5dbedd87f38041c37b667f197854635e8a23c
ming <zzuli4519@163.com>
2016-01-12 22:49:32 +0800

    .

--- app/src/main/java/com/example/ming/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ming/myapplication/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e(TAG,"111");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
