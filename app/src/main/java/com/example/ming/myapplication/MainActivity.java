1709a9d642a172216431f07d4e52840a61da9b80
zzuli4519 <zzuli4519@163.com>
2016-01-12 11:20:09 +0800

    .

--- app/src/main/java/com/example/ming/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ming/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ming.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -10,12 +11,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 测试贝塞尔曲线绘制
  */
 public class MainActivity extends AppCompatActivity {
 
     final String TAG=MainActivity.class.getSimpleName();
+    private View mView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +33,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-                Log.e(TAG, "-----openActivity");
+                Snackbar.make(view, "-----openActivity", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
                 Intent mIntent=new Intent();
                 mIntent.setClass(getApplicationContext(),AboutActivity.class);
                 startActivity(mIntent);
